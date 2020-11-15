639b61986bb7d515694b4d81fe7830b11acbdfd9
df-whu <wudongfang@whu.edu.cn>
2020-11-15 10:20:27 +0800

    1115

---
+++ demo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 15 10:10:35 2020
+
+@author: wh
+"""
+#第一个程序
+a = 1
+b = 2
+sum = a + b
+print("sum is:{}".format(sum))
+print("sum is:{}".format(a+b))
+
